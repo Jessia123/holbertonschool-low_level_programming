6bac81864252a2d0cd154569dce8fdace954bd7c
Jessia123 <jessia.grimes@gmail.com>
2023-01-28 12:35:08 -0800

    hello, print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+ #include <stdio.h>
+ 4
+ 5   // function main begins program execution
+ 6   int main( void )
+ 7   {
+ 8      printf( "with proper grammar, but the outcome is a piece of art,\n" );
+ 9   } // end function main
