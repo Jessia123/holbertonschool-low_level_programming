c5e0c0bb44bc4cadf6f6a7953a33e55a3ce1ad65
Jessia123 <jessia.grimes@gmail.com>
2023-01-28 12:37:49 -0800

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
- #include <stdio.h>
- 4
- 5   // function main begins program execution
- 6   int main( void )
- 7   {
- 8      printf( "with proper grammar, but the outcome is a piece of art,\n" );
- 9   } // end function main
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
