ab0a0288df5a3831b3a0c34208db2196b12486c9
Jessia123 <jessia.grimes@gmail.com>
2023-02-06 21:10:16 -0800

    numbers

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * _isupper - tells if file is uppercase
+ * @c: the variable that prints if uppercase
+ * Return: 1 if uppercase, 0 otherwise
+ */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
@@ -10,4 +16,3 @@ int _isupper(int c)
 		return (0);
 	}
 }
-
