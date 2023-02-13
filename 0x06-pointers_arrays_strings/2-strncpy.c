fed093878cb4b10495cde98474f500c0068c2731
Jessia123 <jessia.grimes@gmail.com>
2023-02-13 13:43:38 -0800

    correction

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,27 +1,29 @@
 #include "holberton.h"
 
 /**
- * *_strncat - main function
+ * *_strncpy - main function
  * @dest: variable
  * @src: variable
  * @n: variable
  * Return: 0
  */
-char *_strncat(char *dest, char *src, int n)
+
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int destlen;
-	/* measures the length of dest*/
-	for (destlen = 0; dest[destlen] != '\0'; destlen++)
-	{}
 
 	/* measures the length from src*/
 	for (i = 0; i < n; i++)
 	{
-		dest[destlen + i] = src[i];
+		dest[i] = src[i];
 		if (src[i] == '\0')
-			break;
+		{
+			while (i < n)
+			{
+				dest[i] = '\0';
+				i++;
+			}
+		}
 	}
-	dest[destlen + i] = '\0';
 	return (dest);
-}
+
