8fac39aa1f3789f8f8b4f5ba75cc042896c54b94
Jessia123 <jessia.grimes@gmail.com>
2023-02-20 15:53:24 -0800

    Print Program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+/*
+ * C Program to Print the Program Name and All its Arguments
+ */
+#include <stdio.h>
+#include "main.h"
+
+void main(int argc, char *argv[])    /* command line Arguments */
+{
+    int i;
+    for (i = 0;i < argc;i++)
+    {
+        printf("%s ", argv[i]);        /* Printing the string */
+    }
+    printf("\n");
+}
+
