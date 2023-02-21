d4846e22d6558f14c240cec6888fae7a264c4c7b
Jessia123 <jessia.grimes@gmail.com>
2023-02-20 16:16:16 -0800

    changing code

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,8 @@
-/*
- * C Program to Print the Program Name and All its Arguments
- */
-#include <stdio.h>
 #include "main.h"
-
-void main(int argc, char *argv[])    /* command line Arguments */
+/*
+*/int main(int argc __attribute__((unused)), char *argv[])
 {
-    int i;
-    for (i = 0;i < argc;i++)
-    {
-        printf("%s ", argv[i]);        /* Printing the string */
-    }
-    printf("\n");
+printf("%s\n", argv[0]);
+return (0);
 }
 
