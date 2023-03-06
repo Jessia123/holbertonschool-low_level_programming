1d71efcee7375d56363ac6bea363b9c88b851a2d
Jessia123 <jessia.grimes@gmail.com>
2023-03-06 15:51:05 -0800

    variadic

--- main.h
+++ main.h
@@ -7,7 +7,7 @@ int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+int _atoi();
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -18,4 +18,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
