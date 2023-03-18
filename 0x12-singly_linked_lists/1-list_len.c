07bcd657d8b54cd4341e07ca2d2453459df339bc
Jessia123 <jessia.grimes@gmail.com>
2023-03-17 20:41:35 -0700

    correction

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+nclude "lists.h"
+
+
+/**
+ * * list_len - check the code
+ * * @c: pointer
+ * * Return: count
+ * */
+
+size_t list_len(const list_t *c)
+{
+		size_t count = 0;
+
+			while (c != NULL)
+					{
+								count++;
+										c = c->next;
+											}
+				return (count);
+}
