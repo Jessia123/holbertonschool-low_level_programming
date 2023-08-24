050ea3cf0353faae11eb8d41b58ca9c90ec948b6
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2023-08-24 03:17:54 +0000

    fix...

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,8 +1,10 @@
 #include "hash_tables.h"
+
 /**
- *hash_table_create - create a new for of hell
- *@size: is the size of hell
- *Return: a finger pointer your direction down
+ * hash_table_create - Creates a new hash table.
+ * @size: The size of the hash table.
+ *
+ * Return: A pointer to the newly created hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -15,10 +17,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (!hash_table->array)
+	{
+		free(hash_table);
 		return (NULL);
+	}
 
-	while (index++ < size)
+	while (index < size)
+	{
 		hash_table->array[index] = NULL;
+		index++;
+	}
 	hash_table->size = size;
 	return (hash_table);
 }
