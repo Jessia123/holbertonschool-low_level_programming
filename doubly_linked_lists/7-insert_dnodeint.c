add8cc86d495ec855f128cf1b9aa126e63f75aea
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2023-11-22 05:32:10 +0000

    fix

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
     dlistint_t *new_node, *current;
-    unsigned int i = 0;
+    unsigned int i;
 
     if (h == NULL)
         return NULL;
@@ -26,19 +26,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
     if (idx == 0)
     {
-        new_node->next = *h;
         if (*h != NULL)
             (*h)->prev = new_node;
+        new_node->next = *h;
         *h = new_node;
         return new_node;
     }
 
     current = *h;
-    while (current != NULL && i < idx - 1)
-    {
+    for (i = 0; i < idx - 1 && current != NULL; i++)
         current = current->next;
-        i++;
-    }
 
     if (current == NULL)
     {
@@ -48,7 +45,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
     new_node->next = current->next;
     new_node->prev = current;
-    
+
     if (current->next != NULL)
         current->next->prev = new_node;
 
@@ -56,3 +53,4 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
     return new_node;
 }
+
