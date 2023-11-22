bf07b0be845efc45ee6e848fc45721ab0a6bd245
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2023-11-22 06:09:36 +0000

    fix

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,44 +1,29 @@
-#ifndef HASH_TABLES_H
-#define HASH_TABLES_H
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#include "hash_tables.h"
 /**
- * struct hash_node_s - Node of a hash table
- * @key: The key, string
- * The key is unique in the HashTable
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
+ * hash_table_create - creates a hash table
+ * @size: size of the hash table
+ * Return: address of hashtable
  */
-typedef struct hash_node_s
+
+hash_table_t *hash_table_create(unsigned long int size)
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
-} hash_node_t;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+	unsigned long int i;
 
-/**
- * struct hash_table_s - Hash table data structure
- * @size: The size of the array
- * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list,
- * because we want our HashTable to use a Chaining collision handling
- */
-typedef struct hash_table_s
-{
-	unsigned long int size;
-	hash_node_t **array;
-} hash_table_t;
-
-hash_table_t *hash_table_create(unsigned long int size);
-unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-char *hash_table_get(const hash_table_t *ht, const char *key);
-void hash_table_print(const hash_table_t *ht);
-void hash_table_delete(hash_table_t *ht);
-
-#endif
+	if (table == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		free(table->array);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+	return (table);
+}
